f48f991c85d0390f0987e9089801bb655e50522e
yusuf <ahmadyusufmaulana0@gmai.com>
2020-12-20 13:47:12 +0700

    ini adalah perubahan pertama

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <div class="header">
+      <a href="/" class="logo">CompanyLogo</a>
+      <div class="header-right">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/contact">Contact</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
@@ -14,17 +20,55 @@
   text-align: center;
   color: #2c3e50;
 }
+/* Style the header with a grey background and some padding */
+.header {
+  overflow: hidden;
+  background-color: #f1f1f1;
+  padding: 8px 40px;
+}
 
-#nav {
-  padding: 30px;
+/* Style the header links */
+.header a {
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 18px;
+  line-height: 25px;
+  border-radius: 4px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+/* Style the logo link (notice that we set the same value of line-height and font-size to prevent the header to increase when the font gets bigger */
+.header a.logo {
+  font-size: 25px;
+  font-weight: bold;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+/* Change the background color on mouse-over */
+.header a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.router-link-exact-active {
+  background-color: dodgerblue;
+  color: white;
+}
+/* Float the link section to the right */
+.header-right {
+  float: right;
+}
+
+/* Add media queries for responsiveness - when the screen is 500px wide or less, stack the links on top of each other */
+@media screen and (max-width: 500px) {
+  .header a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+  .header-right {
+    float: none;
   }
 }
 </style>
